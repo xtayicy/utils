b272f0f40ca776c0734864f8838109b8a19aa838
Harry <www.xtayicy7@163.com>
2018-01-30 18:42:08 +0800

    utils

--- src/main/java/harry/utils/FileUtil.java
+++ src/main/java/harry/utils/FileUtil.java
@@ -1,18 +1,16 @@
 package harry.utils;
 
 import java.io.File;
-import java.io.FileInputStream;
 import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
+import java.io.FileReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.OutputStream;
 import java.io.Reader;
+import java.util.LinkedHashMap;
 import java.util.Map;
+import java.util.Map.Entry;
 
 import org.apache.commons.io.FileUtils;
 import org.apache.commons.io.IOUtils;
-import org.springframework.util.StringUtils;
 
 /**
  * 生活文件工具
@@ -21,38 +19,52 @@ import org.springframework.util.StringUtils;
  *
  */
 public final class FileUtil {
-	private static final int LEN = 1024;
-	private static final byte[] bytes = new byte[LEN];
-	private static final String EMPTY = "";
+	private static final String BLANK = " ";
 
-	private FileUtil() {
+	private FileUtil() {}
+	
+	private static final String getContent(File file) throws IOException{
+		StringBuffer sb = new StringBuffer();
+		char[] buffer = new char[1024];
+		int len;
+		try (Reader reader = new FileReader(file)) {
+			while ((len = IOUtils.read(reader, buffer)) > 0) {
+				sb.append(buffer,0,len);
+			}
+		}
+		
+		return sb.toString();
 	}
-
-	public static final void update(String srcFile, Map<String, String> map) throws FileNotFoundException {
+	
+	public static final String getResult(String srcFile, Map<String, String> map,String prefContent) throws IOException {
 		File file = new File(srcFile);
-		String fileName;
 		if (!file.exists())
 			throw new FileNotFoundException();
-		else if (file.isFile())
-			fileName = file.getName();
-		else
-			return;
+		else if (!file.isFile())
+			throw new FileNotFoundException();
 
 		if (CheckUtils.isNullOrZero(map))
-			return;
+			throw new IllegalArgumentException();
 		
 		
+		Map<String,String> linkedHashMap = new LinkedHashMap<String,String>();
+		for (String blankResults: getContent(file).split(" ")) {
+			linkedHashMap.put(blankResults.split("=")[0], blankResults.split("=")[1]);
+		}
+		
+		for (Entry<String, String> entry : map.entrySet()) {
+			if(linkedHashMap.containsKey(entry.getKey()))
+				linkedHashMap.put(entry.getKey(), entry.getValue());
+		}
+		
+		StringBuffer sb = new StringBuffer(prefContent);
+		for (Entry<String,String> entry : linkedHashMap.entrySet()) {
+			sb.append(entry.getKey()).append("=").append(entry.getValue()).append(BLANK);
+		}
+		
+		return sb.toString();
 	}
 
-	/**
-	 * 复制文件到目的
-	 * 
-	 * @param sourceFile
-	 *            源文件
-	 * @param destDirectories
-	 *            目的目录
-	 * @throws IOException
-	 */
 	public static final void copy(String sourceFile, String... destDirectories) throws IOException {
 		File srcFile = new File(sourceFile);
 		String fileName;
@@ -71,35 +83,5 @@ public final class FileUtil {
 				FileUtils.copyFileToDirectory(srcFile, new File(destDir));
 			}
 		}
-	}
-
-	/**
-	 * 计算文件出现字符串的数量
-	 * 
-	 * @param file
-	 *            文件
-	 * @param string
-	 *            字符串
-	 * @return 数量
-	 * @throws IOException
-	 */
-	public static final int count(File file, String string) throws IOException {
-		int count = 0;
-
-		try (FileInputStream in = new FileInputStream(file)) {
-			StringBuffer s = new StringBuffer(EMPTY);
-			int len = -1;
-			while ((len = in.read(bytes)) != -1) {
-				s.append(new String(bytes, 0, len));
-				while (StringUtil.findString(s.toString(), string)) {
-					count++;
-					s.replace(0, s.length(), s.substring(s.indexOf(string) + string.length()));
-				}
-
-				s.replace(0, s.length(), EMPTY);
-			}
-		}
-
-		return count;
 	}
 }
--- src/test/java/harry/test/FileUtilsTest.java
+++ src/test/java/harry/test/FileUtilsTest.java
@@ -1,11 +1,33 @@
 package harry.test;
 
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
 import org.junit.Test;
 
+import harry.utils.FileUtil;
+
 public class FileUtilsTest {
+	
+	/**
+	 * @throws IOException
+	 */
 	@Test
-	public void test() throws IOException{
+	public void test() throws IOException {
+		String fileName = "maven-war-plugin-2.6.jar";
+		String groupId = "org.apache.maven.plugins";
+		String artifactId = "maven-war-plugin";
+		String version = "2.6";
 		
+		FileUtil.copy("D:\\download\\" + fileName, 
+				      "F:\\test");
+		Map<String,String> hashMap = new HashMap<String,String>();
+		hashMap.put("-DgroupId", groupId);
+		hashMap.put("-DartifactId", artifactId);
+		hashMap.put("-Dversion", version);
+		hashMap.put("-Dfile", "F:\\test\\" + fileName);
+		String result = FileUtil.getResult("test.txt",hashMap, "mvn deploy:deploy-file ");
+		System.out.println(result);
 	}
 }
