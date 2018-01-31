f545299cd08692970fd51d53edc314142fe8162e
xtayicy <www.xtayicy7@163.com>
2018-01-31 13:30:00 +0800

    优化

--- src/main/java/harry/utils/MathUtil.java
+++ src/main/java/harry/utils/MathUtil.java
@@ -8,29 +8,28 @@ import java.util.List;
  *
  */
 public final class MathUtil {
-	private MathUtil(){}
-	
-	public static boolean isArithmeticProgression(List<Integer> list){
-		Integer temp = null,subtract = null;
+	private MathUtil() {
+	}
+
+	public static boolean isArithmeticProgression(int[] arrs) {
+		Integer temp = null, gap = null;
 		boolean flag = false;
-		int index = list != null ? list.size() - 1 : 0;
-		
-		while(index > 1){
-			subtract = list.get(index) - list.get(index - 1);
-			if(list.size() - 1 == index){
-				temp = subtract;
-			}else{
-				if(subtract != null && subtract != temp){
-					return false;
-				}
+		int index = arrs == null ? 0 : arrs.length - 1;
+
+		while (index > 1) {
+			gap = arrs[index] - arrs[index - 1];
+			if (arrs.length - 1 == index) {
+				temp = gap;
+			} else {
+				return !(gap != null && gap != temp);
 			}
-			
+
 			index--;
-			subtract = null;
-			if(index == 1)
-				flag = true;
+			gap = null;
+
+			flag = index == 1;
 		}
-		
+
 		return flag;
-	} 
+	}
 }
